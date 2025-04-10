562bbf28a6eeea1e234baa9a351bae8889320377
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-04-10 14:41:18 +0800

    fix class

--- resources/views/frontend/scheduledConference/components/committees-toc.blade.php
+++ resources/views/frontend/scheduledConference/components/committees-toc.blade.php
@@ -2,7 +2,7 @@
 	@foreach (App\Models\CommitteeRole::get() as $role)
 		@if ($role->committees->isNotEmpty())
 			<div class="w-auto sm:max-w-screen-xl mx-4 lg:mx-auto">
-				<h2 class="committees-title text-3xl sm:text-6xl font-bold text-primary">
+				<h2 class="committee-role-title text-3xl sm:text-6xl font-bold text-primary">
 					{{ $role->name }}
 				</h2>
 				<div class="committees-slider mt-6 sm:mt-12 py-4 flex gap-6 sm:gap-12 overflow-x-scroll">
