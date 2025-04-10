1d8dde865f53589d7702056d77dc649e1e2c5f40
Rahman Ramsi <rahmanramsi19@gmail.com>
2025-04-10 14:39:58 +0800

    fix class

--- resources/views/frontend/scheduledConference/components/committees-toc.blade.php
+++ resources/views/frontend/scheduledConference/components/committees-toc.blade.php
@@ -1,11 +1,11 @@
-<div class="speakers py-10 sm:py-20 space-y-12">
+<div class="committees py-10 sm:py-20 space-y-12">
 	@foreach (App\Models\CommitteeRole::get() as $role)
 		@if ($role->committees->isNotEmpty())
 			<div class="w-auto sm:max-w-screen-xl mx-4 lg:mx-auto">
-				<h2 class="speakers-title text-3xl sm:text-6xl font-bold text-primary">
+				<h2 class="committees-title text-3xl sm:text-6xl font-bold text-primary">
 					{{ $role->name }}
 				</h2>
-				<div class="speakers-slider mt-6 sm:mt-12 py-4 flex gap-6 sm:gap-12 overflow-x-scroll">
+				<div class="committees-slider mt-6 sm:mt-12 py-4 flex gap-6 sm:gap-12 overflow-x-scroll">
 					@foreach ($role->committees as $committee)
 						<x-scheduledConference::committee-summary :committee="$committee" />
 					@endforeach
